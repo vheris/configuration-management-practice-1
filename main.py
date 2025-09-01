9c72dc99c28e1cbd71427ad191664f2e4cc8ebcf
vheris <semann1929@gmail.com>
2025-09-01 16:30:20 +0300

    Этап 1. REPL (1-5)

---
+++ main.py
@@ -0,0 +1,47 @@
+import tkinter
+import os
+import platform
+
+root = tkinter.Tk()
+
+root.title('VFS')
+
+root.geometry('800x400')
+
+history = tkinter.Text(root)
+history.config(state='disabled')
+history.pack()
+
+commands = tkinter.Entry(root, width=40)
+commands.pack()
+commands.bind("<Return>", lambda event: view())
+
+
+def view():
+    cmd = commands.get()
+    history.config(state='normal')
+    history.insert(tkinter.END, f"{platform.node()} ~ % {cmd}\n")
+    parts = cmd.split()
+    if not parts:
+        history.insert(tkinter.END, f"{platform.node()} ~ % empty command\n")
+    elif parts[0] == 'ls':
+        history.insert(tkinter.END, f"ls: {parts[1:]}\n")
+    elif parts[0] == 'cd':
+        history.insert(tkinter.END, f"cd: {parts[1:]}\n")
+    elif cmd.startswith("$"):
+        parser()
+    else:
+        history.insert(tkinter.END, f"{platform.node()} ~ % unknown command\n")
+    commands.delete(0, tkinter.END)
+    history.config(state='disabled')
+
+def parser():
+    cmd = commands.get()
+    varname = cmd[1:]
+    if varname in os.environ:
+        cmd = os.path.expandvars(cmd)
+        history.insert(tkinter.END, f"{platform.node()} ~ % {cmd}\n")
+    else:
+        history.insert(tkinter.END, f"{platform.node()} ~ % unknown command\n")
+
+root.mainloop()
